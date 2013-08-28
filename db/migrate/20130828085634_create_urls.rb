a56e1f99aca306c67c6f408f6eda48e9520e5cc9
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 10:04:12 -0500

    first commit

---
+++ db/migrate/20130828085634_create_urls.rb
@@ -0,0 +1,10 @@
+class CreateUrls < ActiveRecord::Migration
+  def change
+    create_table :urls do |t|
+      t.string :url
+      t.string :short_url
+      t.integer :clicks
+      t.timestamps
+    end
+  end
+end
