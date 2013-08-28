e94407ff2d80cf3a4f7779dfcebef221df9f700a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 13:48:02 -0500

    something nice

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,12 +4,31 @@ get '/' do
 end
 
 post '/urls' do
-  puts params[:urls]
+  params[:urls]
   @url = Url.new(params[:urls])
   @short_url = @url.generate_short_url
+  @url.url 
+  @url.short_url = @short_url
+  @url.clicks = 0
+  @url.save
 
   #  Url.create(:url => )
   #### stick in in the urls table with url and short_url
   erb :urls
 end 
+
+get '/:short_url' do
+  #### find row for short_url
+    #### count += 1
+    #### redirect to url
+  @look_url = 'shit.ly/' + params[:short_url]
+  current_click = Url.where(short_url: @look_url).first.clicks
+  current_click += 1
+  @url_clicks = current_click
+  @url.save
+  @redirect_url = Url.where(short_url: @look_url).first.url
+
+  erb :show
+end
+
 
