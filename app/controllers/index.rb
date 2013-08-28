a4814ee493d5b60c72b29007e4a775cbdd35e481
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 10:11:39 -0500

    sinatra skeleton --b

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,14 @@ get '/' do
   erb :index
 end
 
-
 post '/urls' do
+  @short_url = generate_short_url
   erb :urls
 end 
+
+def generate_short_url
+  chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789'
+  password = ''
+  6.times { |i| password << chars[rand(chars.length)] }
+  "shit.ly/" + password
+end
